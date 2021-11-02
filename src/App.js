7ec2d9defa88ade6034ada788d157d0e68ec1094
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-02 14:08:52 -0500

    ran npm install

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React from "react";
+import React from 'react'
 
-import { Route, Switch, Redirect } from "react-router-dom";
-import { connect } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from './components/MovieList'
+import Movie from './components/Movie'
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from './components/MovieHeader'
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from './components/AddMovieForm'
+import FavoriteMovieList from './components/FavoriteMovieList'
 
-const App = props => {
-  const displayFavorites = true;
+const App = (props) => {
+  const displayFavorites = true
 
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/>Redux Module Project</span>
+      <nav className='navbar navbar-dark bg-dark'>
+        <span className='navbar-brand'>
+          <img width='40px' alt='' src='./Lambda-Logo-Red.png' />
+          Redux Module Project
+        </span>
       </nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          {displayFavorites && <FavoriteMovieList/>}
-        
+      <div className='container'>
+        <MovieHeader />
+        <div className='row '>
+          {displayFavorites && <FavoriteMovieList />}
+
           <Switch>
-            <Route exact path="/movies/add">
+            <Route exact path='/movies/add'>
               <AddMovieForm />
             </Route>
 
-            <Route path="/movies/:id">
+            <Route path='/movies/:id'>
               <Movie />
             </Route>
 
-            <Route path="/movies">
-              <MovieList/>
+            <Route path='/movies'>
+              <MovieList />
             </Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
+            <Route path='/'>
+              <Redirect to='/movies' />
             </Route>
           </Switch>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
